b66654fbbaded97a6f9c5a4943710d3f2bd27480
UdaySantoshKumar <75845600+udaysk3@users.noreply.github.com>
2023-03-30 00:21:23 +0530

    Delete angular-eCart-master directory

--- angular-eCart-master/src/app/config/app-congif.ts
+++
@@ -1,7 +0,0 @@
-import { environment } from '../../environments/environment';
-
-export const baseUrl = environment.production ? 'https://angular-ecart.firebaseio.com/' : 'https://angular-ecart.firebaseio.com/'
-
-export const productUrl = baseUrl + '/products.json';
-export const cartUrl = baseUrl + '/carts.json';
-export const wishlistUrl = baseUrl + '/wishlist.json';
